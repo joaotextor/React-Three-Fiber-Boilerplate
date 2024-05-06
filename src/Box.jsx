879b4d6f3d2efff83a608dd3aaa016d2e8c72262
João Textor <joaotextor@gmail.com>
2024-05-06 15:34:20 -0300

    box element

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,12 +1,46 @@
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import keyGen from 'easy-key-generator'
+
+const setRandomColor = () => {
+  const colorGen = new keyGen(8, { characterType: 'HexChar' })
+  return parseInt(`0x${colorGen.generate()}`.toLowerCase())
+}
 
 const Box = ({ wireframe, ...props }) => {
-  const ref = useRef()
-  console.log(ref)
+  const ref = useRef({})
+  const [hovered, setHover] = useState(false)
+  const [rotate, setRotate] = useState(false)
+  // const speed = 0.5
+  // const amplitude = 1
+
+  useFrame((state, delta) => {
+    if (rotate) {
+      ref.current.rotation.x += 1 * delta
+      ref.current.rotation.y += 0.5 * delta
+    }
+
+    // ref.current.position.y =
+    //   Math.sin(state.clock.getElapsedTime() * speed) * amplitude
+  })
+
+  useEffect(() => {
+    console.log(ref)
+  }, [])
   return (
-    <mesh {...props} ref={ref}>
+    <mesh
+      {...props}
+      ref={ref}
+      scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+      onPointerDown={() => setRotate(!rotate)}
+      onPointerUp={() => setRotate(!rotate)}
+      onPointerOver={() => setHover(!hovered)}
+      onPointerOut={() => setHover(!hovered)}>
       <boxGeometry />
-      <meshBasicMaterial color={0x00ff00} wireframe={wireframe} />
+      <meshBasicMaterial
+        color={hovered ? setRandomColor() : 0x00ff00}
+        wireframe={wireframe}
+      />
     </mesh>
   )
 }
