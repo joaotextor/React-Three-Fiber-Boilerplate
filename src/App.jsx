e22c763a1bd63bed7f737459be98470bdcaf8f78
João Textor <joaotextor@gmail.com>
2024-05-10 11:35:45 -0300

    add labels and light

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,68 @@
+import {
+  Stats,
+  OrbitControls,
+  Circle,
+  Environment,
+  Html
+} from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-import { Stats, OrbitControls } from '@react-three/drei'
-import Polyhedron from './Polyhedron'
-import * as THREE from 'three'
+import { useGLTF } from '@react-three/drei'
+import { useState } from 'react'
+
+function Model() {
+  const url = '/models/small_room.glb'
+  const { scene } = useGLTF(url)
+  const [cache, setCache] = useState({})
+
+  if (!cache[url]) {
+    const annotations = []
+    scene.traverse((i) => {
+      if (i.isMesh) {
+        i.castShadow = true
+        i.receiveShadow = true
+      }
+
+      if (i.userData.prop) {
+        annotations.push(
+          <Html
+            key={i.uuid}
+            position={[i.position.x, i.position.y, i.position.z]}
+            distanceFactor={2}>
+            <div className="annotation">{i.userData.prop}</div>
+          </Html>
+        )
+      }
+    })
+    setCache({
+      ...cache,
+      [url]: (
+        <primitive object={scene} castShadow receiveShadow>
+          {annotations}
+        </primitive>
+      )
+    })
+  }
+
+  return cache[url]
+}
 
 export default function App() {
-  const polyhedron = [
-    new THREE.BoxGeometry(),
-    new THREE.SphereGeometry(0.785398),
-    new THREE.DodecahedronGeometry(0.785398)
-  ]
-
   return (
-    <Canvas camera={{ position: [0, 0, 3] }}>
-      <Polyhedron position={[-0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
-      <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+      <Environment preset="sunset" background />
+      <spotLight
+        position={[15, 20, 10.5]}
+        angle={1}
+        power={10000}
+        distance={100}
+        castShadow>
+        <mesh>
+          <sphereGeometry args={[0.25]} />
+        </mesh>
+      </spotLight>
+      <Model />
+      <OrbitControls target={[0, 1, 0]} />
       <Stats />
-      <OrbitControls
-        enableDamping
-        enableZoom={false}
-        minAzimuthAngle={-Math.PI / 8}
-        maxAzimuthAngle={Math.PI / 8}
-        minPolarAngle={Math.PI / 3}
-        maxPolarAngle={Math.PI - Math.PI / 3}
-      />
     </Canvas>
   )
 }
